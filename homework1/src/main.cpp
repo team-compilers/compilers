2911985d4e8a41fd0de6bcf86f1b42a33e587ca3
Alexander Smirnov <smirnov@phytech.edu>
2016-09-15 13:02:56 +0400

    Add test and image

--- homework1/src/main.cpp
+++ homework1/src/main.cpp
@@ -1,6 +1,42 @@
+#include <string>
+#include <iostream>
+#include <fstream>
 
 #include <PrintVisitor.h>
+#include <Statement.h>
+#include <Expression.h>
+#include <VisitorTarget.h>
+
+CAssignStatement* CreateTestTree() {
+    CIdExpression* expressionA = new CIdExpression();
+    CBinaryExpression* binaryExpression = new CBinaryExpression();
+    CNumberExpression* numberExpressionFive = new CNumberExpression();
+    CNumberExpression* numberExpressionThree = new CNumberExpression();
+    CAssignStatement* assignStatement = new CAssignStatement();
+
+    expressionA->Name = "a";
+
+    assignStatement->LeftValue = expressionA;
+    assignStatement->RightValue = binaryExpression;
+
+    numberExpressionFive->Value = 5;
+    numberExpressionThree->Value = 3;
+
+    binaryExpression->LeftOperand = numberExpressionFive;
+    binaryExpression->Operation = TOperandType::OT_Plus;
+    binaryExpression->RightOperand = numberExpressionThree;
+
+    return assignStatement;
+}
 
 int main( int argc, char** argv ) {
+    CAssignStatement* treeRoot = CreateTestTree();
+    CPrintVisitor printVisitor;
+    printVisitor.Visit( treeRoot );
+    std::string result = printVisitor.GetTraversalInDotLanguage();
+    std::ofstream outStream("example1.gv");
+    outStream << result;
+    outStream.close();
+
     return 0;
 }
