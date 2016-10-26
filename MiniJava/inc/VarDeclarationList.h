a18c23c7111572d5f9b78fbbb4941809bc478357
Andrew Sautin <sautin1@yandex.ru>
2016-10-27 01:43:03 +0300

    CVarDeclaration moved to separate file

--- MiniJava/inc/VarDeclarationList.h
+++ MiniJava/inc/VarDeclarationList.h
@@ -4,26 +4,8 @@
 #include <Visitor.h>
 #include <TypeModifier.h>
 #include <Expression.h>
+#include <VarDeclaration.h>
 #include <vector>
-
-class CVarDeclaration : public IVisitorTarget {
-public:
-
-    CVarDeclaration( const ITypeModifier* _type, const CIdExpression* _id )
-        : type( _type ),
-          id( _id ) {}
-
-    const ITypeModifier* Type() const { return type; }
-    const CIdExpression* Id() const { return id; }
-
-    void Accept( IVisitor* visitor ) override { visitor->Visit( this ); }
-
-private:
-    std::unique_ptr<const ITypeModifier> type;
-    std::unique_ptr<const CIdExpression> id;
-};
-
-//---------------------------------------------------------------------------------------//
 
 class CVarDeclarationList : public IVisitorTarget {
 public:
