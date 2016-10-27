9fd5e8add032082e4a39986a8e15a0a4fa2b898b
Журавлев Алексей Алексеевич <aa_Zhur@mail.ru>
2016-10-27 13:52:44 +0300

    More fixes and additions to visitor interface

--- MiniJava/inc/ClassDeclaration.h
+++ MiniJava/inc/ClassDeclaration.h
@@ -31,6 +31,8 @@ public:
     bool HasParent() const { return hasParent; }
     const CIdExpression* ExtendsClassName() const { return extendsClassName.get(); } 
 
+    void Accept( IVisitor* visitor) override { visitor->Visit( this ); }
+
 private:
     std::unique_ptr<const CIdExpression> className;
     std::unique_ptr<const CVarDeclarationList> varDecls;
--- MiniJava/inc/Visitor.h
+++ MiniJava/inc/Visitor.h
@@ -20,6 +20,21 @@ class CPrintStatement;
 class CConditionalStatement;
 class CWhileLoopStatement;
 class CBracesStatement;
+class CPublicAccessModifier;
+class CPrivateAccessModifier;
+class CMethodArgument;
+class CMethodDeclaration;
+class CIntTypeModifier;
+class CBooleanTypeModifier;
+class CIntArrayTypeModifier;
+class CIdTypeModifier;
+class CVarDeclaration;
+class CClassDeclaration;
+class CProgram;
+class CStatementList;
+class CExpressionList;
+class CMethodDeclarationList;
+class CClassDeclarationList;
 
 class IVisitor {
 public:
@@ -40,4 +55,20 @@ public:
     virtual void Visit( CConditionalStatement* statement ) = 0; 
     virtual void Visit( CWhileLoopStatement* statement ) = 0; 
     virtual void Visit( CBracesStatement* statement ) = 0; 
+    virtual void Visit( CPrivateAccessModifier* modifier ) = 0;
+    virtual void Visit( CPublicAccessModifier* modifier ) = 0;
+    virtual void Visit( CMethodArgument* argument ) = 0;
+    virtual void Visit( CMethodDeclaration* declaration ) = 0;
+    virtual void Visit( CIntTypeModifier* intModifier ) = 0;
+    virtual void Visit( CBooleanTypeModifier* booleanModifier ) = 0;
+    virtual void Visit( CIntArrayTypeModifier* intArrayModifier ) = 0;
+    virtual void Visit( CIdTypeModifier* idModifier ) = 0;
+    virtual void Visit( CClassDeclaration* classDeclaration ) = 0;
+    virtual void Visit( CVarDeclaration* varDeclaration ) = 0;
+    virtual void Visit( CProgram* program ) = 0;
+    virtual void Visit( CStatementList* list ) = 0;
+    virtual void Visit( CExpressionList* list ) = 0;
+    virtual void Visit( CMethodArgumentList* list ) = 0;
+    virtual void Visit( CMethodDeclarationList* list ) = 0;
+    virtual void Visit( CClassDeclarationList* list ) = 0;
 };
