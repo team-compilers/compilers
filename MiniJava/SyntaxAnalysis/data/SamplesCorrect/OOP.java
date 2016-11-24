2b601526041b55539a177a92a057bfcc614ef909
Andrew Sautin <sautin1@yandex.ru>
2016-11-25 01:38:46 +0300

    Test case for class hierarchy searching added

---
+++ MiniJava/SyntaxAnalysis/data/SamplesCorrect/OOP.java
@@ -0,0 +1,23 @@
+class Main{
+    public static void main(String[] a){
+	   System.out.println(new GrandChild().Func(2));
+    }
+}
+
+class Base {
+    int size;
+
+    public int Func(int num){
+	   return 1;
+    }
+}
+
+class Child extends Base {
+
+}
+
+class GrandChild extends Child {
+    private int Add() {
+        return size + 1;
+    }
+}
